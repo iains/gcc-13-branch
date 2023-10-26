f23a3e866139b0ef2a1ee1819fa762002fcb3637
Iain Sandoe <iain@sandoe.co.uk>
2023-10-26 10:57:43 +0100

    testsuite, aarch64, Darwin: Unsorted changes
    (cherry picked from commit b24ba7c69f9cc5edf5c02f203994f79b903290c4)

--- gcc/testsuite/gcc.target/aarch64/pr109661-1.c
+++ gcc/testsuite/gcc.target/aarch64/pr109661-1.c
@@ -1,5 +1,12 @@
+/* { dg-skip-if "Darwin ABI is different, test separately" { *-*-darwin* } } */
 /* { dg-options "-O2 -Wpsabi" } */
+/* { dg-final { check-function-bodies "**" "" "" } } */
 
 enum __attribute__((aligned(16))) e { E };
 
+/*
+** test:
+**	mov	w0, w1
+**	ret
+*/
 enum e test (int x, enum e y) { return y; }
